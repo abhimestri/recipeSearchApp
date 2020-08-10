0a92138fd33ea261a236ff5de975a6d4d994d3cd
Jeevan Surendran <jvns67@gmail.com>
2020-08-10 21:34:20 +0530

    Fixed reducer related bugs
    The issue you faced was related to a small bug you introduced in RECERECEPIENAME_UPDATED. Basically that reducer which would actually not mutate the existing state but instead create a new object with just inputSearchedRecipe so the store would look something like { inputSearchedRecepiE: cake } and the fauvist and eachRecpie was gone.
    
    Second error that you created was related to ADDTOFAVORITES where you used initialState instead of state and hence when you add a new faviorite the inputSeachRecpie would become null and the project would have no content to render. Even each receipt would become null.
    
    Pls use the chrome extension react dev tools, its super useful and would make your dev easier

--- src/Store/reducer/reducer.js
+++ src/Store/reducer/reducer.js
@@ -1,30 +1,30 @@
 import * as actionType from '../actions/actionTypes'
 
 const initialState = {
-    aray : [],
-    inputSearchedRecipe : null,
-    eachRecipe : null
+    favList: [],
+    inputSearchedRecipe: null,
+    eachRecipe: null
 }
 
 const reducer = (state = initialState, action) => {
 
-    switch(action.type){
-        case actionType.RECEPIENAME_UPDATED : return {
-            inputSearchedRecipe : action.recipeName
+    switch (action.type) {
+        case actionType.RECEPIENAME_UPDATED: return {
+            ...state,
+            inputSearchedRecipe: action.recipeName
         }
-        case actionType.EACHRECIPE_DETAILS : 
+        case actionType.EACHRECIPE_DETAILS:
             return {
                 ...state,
-                eachRecipe : action.recipeData
+                eachRecipe: action.recipeData
             }
-        case actionType.ADDTOFAVORITES :
-                let ONG = action.favoritesListArray
-                return {
-                    ...initialState,
-                    aray : initialState.aray.concat(ONG)
-                }
-            
-        default : return state
+        case actionType.ADDTOFAVORITES:
+            return {
+                ...state,
+                favList: state.favList.concat(action.favoritesListArray)
+            }
+
+        default: return state
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import reducer from './Store/reducer/reducer'
-import {createStore} from 'redux'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-const store = createStore(reducer)
+const store = createStore(reducer, process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-    <Provider store={store} >
-      <BrowserRouter>
-        <React.StrictMode>
-          <App/>    
-        </React.StrictMode>
-      </BrowserRouter>
-    </Provider>,
+  <Provider store={store} >
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
